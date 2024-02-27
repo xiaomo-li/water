554c2012996fe90527c25c86f3c4388fe7983b78
xiaomo-li <867416257@qq.com>
2024-02-27 23:16:07 +0800

    可360度旋转

--- src/components/WaterPage.vue
+++ src/components/WaterPage.vue
@@ -7,7 +7,7 @@ import * as THREE from "three";
 import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 export default {
   name: "WaterPage",
   mounted() {
@@ -16,29 +16,29 @@ export default {
   methods: {
     init() {
       const scene = new THREE.Scene();
-      scene.background = new THREE.Color(0xa0a0a0);
-      const camera = new THREE.PerspectiveCamera(
+      // scene.background = new THREE.Color(0xaaaaaa);
+      let camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth / window.innerHeight,
-        0.1,
-        1000
+        1,
+        5000
       );
-      camera.position.set(100, 200, 300);
-      const renderer = new THREE.WebGLRenderer();
+      camera.position.set(2500, 1000, 100);
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
-      let water = document.getElementById("water");
-      water.appendChild(renderer.domElement);
+      document.getElementById("water").appendChild(renderer.domElement);
+
       // 环境光
-      // const light = new THREE.AmbientLight(0xffffff, 5);
-      // scene.add(light);
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+      scene.add(ambientLight);
       // 半球光：天空和地板颜色之间的渐变光
       const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
-      // hemiLight.position.set(0, 200, 0);
+      hemiLight.position.set(0, 200, 0);
       scene.add(hemiLight);
       // 定向光：互相平行的太阳光线
       const dirLight = new THREE.DirectionalLight(0xffffff, 5);
-      // dirLight.position.set(0, 500, 100);
-      // dirLight.castShadow = true;
+      dirLight.position.set(0, 500, 100);
+      dirLight.castShadow = true;
       // dirLight.shadow.camera.top = 180;
       // dirLight.shadow.camera.bottom = -100;
       // dirLight.shadow.camera.left = -120;
@@ -50,17 +50,16 @@ export default {
       loader.load("/1.fbx", function (object) {
         scene.add(object);
       });
-      // window.addEventListener("resize", onWindowResize);
 
-      // function onWindowResize() {
-      //   camera.aspect = window.innerWidth / window.innerHeight;
-      //   camera.updateProjectionMatrix();
-
-      //   renderer.setSize(window.innerWidth, window.innerHeight);
-      // }
+      new RGBELoader().load("/studio012.hdr"),
+        function (texture) {
+          texture.mapping = THREE.EquirectangularReflectionMapping;
+          scene.environment = texture;
+          renderer.outputEncoding = THREE.sRGBEncoding;
+          renderer.render(scene, camera);
+        };
 
       const controls = new OrbitControls(camera, renderer.domElement);
-      controls.target.set(0, 100, 0);
       controls.update();
 
       function animate() {
